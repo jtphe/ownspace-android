71ee1f726efe4cc848da335b044bac315620d359
Jean <jean.phe.fr@gmail.com>
2020-03-26 21:30:15 +0100

    feat(add-authentication): Create the login screen
    feat(add-authentication): Add authentication function
    
    - Fix the text edit display
    - Redirect the need help text view button to email
    
    feat(add-authentication): Can sign in
    
    - Create Authentication Activity
    - Fix the authentication module

--- app/src/main/java/com/example/ownspace/MainActivity.kt
+++ app/src/main/java/com/example/ownspace/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.ownspace
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.client.Callback
+import com.amazonaws.mobile.client.results.SignInResult
+import com.amazonaws.mobile.client.results.SignInState
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
+
